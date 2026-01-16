929f8412976c5314c011faa6d72bf14998afc98a
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2026-01-17 01:23:57 +0400

    fixup builder

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -59,7 +59,7 @@ prepare_workdir(){
 		git clone $mesasrc --depth=1 --no-single-branch $srcfolder
 		cd $srcfolder
 	echo "Pushing TU_VERSION..."
-		echo "#define TUGEN8_DRV_VERSION \"v$BUILD_VERSION\" > ./src/freedreno/vulkan/tu_version.h
+		echo "#define TUGEN8_DRV_VERSION \"v$BUILD_VERSION\"" > ./src/freedreno/vulkan/tu_version.h
 }
 
 
