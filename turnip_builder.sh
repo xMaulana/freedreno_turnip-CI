f95913e91c4cda69427bd4a3ff1b8d659ef04dcd
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2026-03-28 23:48:04 +0400

    Feat[builder]: disable LTO

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -117,8 +117,6 @@ EOF
 			--native-file "native.txt" \
 			--prefix /tmp/turnip-$1 \
 			-Dbuildtype=release \
-			-Db_lto=true \
-   			-Db_lto_mode=thin \
 			-Dstrip=true \
 			-Dplatforms=android \
 			-Dvideo-codecs= \
